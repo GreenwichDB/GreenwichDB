a29a23663258f1aea8f498afe7818c4e2c72993d
Milo <50248166+Milo123459@users.noreply.github.com>
2021-07-22 18:55:47 +0100

    Setup selene

--- greenwich.lua
+++ greenwich.lua
@@ -10,20 +10,23 @@ function greenwich:GetDB(name)
     local db = dss:GetDataStore(name)
     local new = {}
     new.store = db
-    for k, v in ipairs(dbFunctions) do
-       new[k] = function(...)
-            local args = { ... }
-            v(new.store, unpack(args))
-       end
-    end
+    coroutine.resume(coroutine.create(function()
+        for k, v in ipairs(dbFunctions) do
+            new[k] = function(...)
+
+                local args = { ... }
+                v(new.store, unpack(args))
+
+            end
+
+        end
+    end))
     return new
 end
 
 function dbFunctions:Set(store, key, value)
     return value
 end
-
-greenwich:GetDB("Greenwich")
 
 --Returning everything.
 return greenwich
