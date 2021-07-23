f96330a068bf8a55e054991b0f7ab7e83612dfd3
Milo <50248166+Milo123459@users.noreply.github.com>
2021-07-23 16:06:11 +0100

    Add get method

--- greenwich.lua
+++ greenwich.lua
@@ -9,19 +9,28 @@ local dbFunctions = {}
 -- Functions
 function greenwich:GetDB(name)
     local new = {}
-    new.key = name
+    new.name = name
     coroutine.resume(coroutine.create(function()
         for k, v in pairs(dbFunctions) do
             new[k] = function(...)
                 local args = {...}
-                return v(new.key, unpack(args))
+                return v(unpack(new), unpack(args))
             end
         end
     end))
     return new
 end
 
-function dbFunctions:Set(save_key, key, value) return value end
+function dbFunctions:Set(store, key, value)
+    store = store.name
+    db:SetAsync(store .. key, value)
+    return value
+end
+
+function dbFunctions:Get(store, key)
+    store = store.name
+    return db:GetAsync(store .. key)
+end
 
 -- Returning everything.
 return greenwich
