900f7641b24abb00fc0d1b75e705cc59a00857c4
Milo <50248166+Milo123459@users.noreply.github.com>
2021-07-27 13:30:33 +0100

    Basic way of saving data in 1 place and only updating the cache
    Co-authored-by: NoobieYT <66630098+NoobieYT@users.noreply.github.com>

--- greenwich.lua
+++ greenwich.lua
@@ -1,6 +1,6 @@
 -- Variables
 local dss = game:GetService("DataStoreService")
-local db = dss:GetDataStore("greenwich")
+local db = dss:GetDataStore("Greenwich")
 
 -- Tables
 local greenwich = {}
@@ -9,59 +9,71 @@ local cache = {}
 
 -- Functions
 function greenwich:GetDB(name)
-    local new = {}
-    new.name = name
-    coroutine.resume(coroutine.create(function()
-        for k, v in pairs(dbFunctions) do
-            local fn = function(...)
-                local args = {...}
-                return v(unpack(new), unpack(args))
-            end
-            new[k] = fn
-            new[string.lower(k)] = fn
-        end
-    end))
-    return new
+	local new = {}
+	new.name = name
+	coroutine.resume(coroutine.create(function()
+		for k, v in pairs(dbFunctions) do
+			local fn = function(...)
+				local args = {...}
+				return v(unpack(new), unpack(args))
+			end
+			new[k] = fn
+			new[string.lower(k)] = fn
+		end
+	end))
+	return new
 end
 
-function dbFunctions:Set(store, key, value)
-    store = store.name
-    cache[store .. key] = value
-    db:SetAsync(store .. key, value)
-    return value
+function dbFunctions:Set(store, key, value, shouldSave)
+	store = store.name
+	cache[store .. key] = value
+	if shouldSave == nil or shouldSave == true then
+		db:SetAsync(store .. key, value)
+	end
+	return value
 end
 
 function dbFunctions:Get(store, key)
-    store = store.name
-    if not not cache[store .. key] then 
-        return cache[store .. key]
-    else
-        local val = db:GetAsync(store .. key)
-        cache[store .. key] = val
-        return val
-    end
+	store = store.name
+	if not not cache[store .. key] then 
+		return cache[store .. key]
+	else
+		local val = db:GetAsync(store .. key)
+		cache[store .. key] = val
+		return val
+	end
 end
 
 function dbFunctions:Delete(store, key)
-    store = store.name
-    local success, val = pcall(function()
-        return db:RemoveAsync(store .. key)
-    end)
-    cache[store .. key] = nil
-    if val and success then
-        return true
-    else
-        return false
-    end
+	store = store.name
+	local success, val = pcall(function()
+		return db:RemoveAsync(store .. key)
+	end)
+	cache[store .. key] = nil
+	if val and success then
+		return true
+	else
+		return false
+	end
 end
 
 function dbFunctions:Has(store, key)
-    store = store.name
-    if not not cache[store .. key] then
-        return not not cache[store .. key]
-    else
-    return not not db:GetAsync(store .. key)
-    end
+	store = store.name
+	if not not cache[store .. key] then
+		return not not cache[store .. key]
+	else
+		return not not db:GetAsync(store .. key)
+	end
+end
+
+function dbFunctions:Save(store, key) 
+	store = store.name
+	if cache[store .. key] == nil then
+		return false
+	else
+		db:SetAsync(store .. key, cache[store .. key])
+		return true
+	end
 end
 
 -- Returning everything.
