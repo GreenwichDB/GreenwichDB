26ecec96d46bfb4e2458313e0f38f5571e8a974f
frames <68570351+workframes@users.noreply.github.com>
2021-12-04 01:39:43 -0800

    Update greenwich.lua (#7)

--- greenwich.lua
+++ greenwich.lua
@@ -4,135 +4,123 @@ local db = dss:GetDataStore("Greenwich")
 
 -- Tables
 local greenwich = {}
-local dbFunctions = {}
+greenwich.__index = greenwich
+
 local cache = {}
 local queue = {}
 
 -- Helper function
 
 function addToQueue(fn)
-    queue[os.time()] = fn
+	queue[os.time()] = fn
 end
 
 -- Functions
-function greenwich:GetDB(name)
-    local new = {}
-    new.name = name
-    coroutine.resume(
-        coroutine.create(
-            function()
-                for k, v in pairs(dbFunctions) do
-                    local fn = function(...)
-                        local args = {...}
-                        return v(unpack(new), unpack(args))
-                    end
-                    new[k] = fn
-                    new[string.lower(k)] = fn
-                end
-            end
-        )
-    )
-    return new
+function greenwich.GetDB(name)
+	local new = setmetatable({}, greenwich)
+	new.name = name
+	
+	return new
 end
 
-function dbFunctions:Set(store, key, value, shouldSave)
-    store = store.name
-    cache[store .. key] = value
-    if shouldSave == nil or shouldSave == true then
-        addToQueue(
-            function()
-                db:SetAsync(store .. key, value)
-            end
-        )
-    end
-    return value
+function greenwich:Set(key, value, shouldSave)
+	local store = self.name
+	cache[store .. key] = value
+	if shouldSave == nil or shouldSave == true then
+		addToQueue(
+			function()
+				db:SetAsync(store .. key, value)
+			end
+		)
+	end
+	return value
 end
 
-function dbFunctions:Get(store, key)
-    store = store.name
-    if not (not cache[store .. key]) then
-        return cache[store .. key]
-    else
-        local val = db:GetAsync(store .. key)
-        cache[store .. key] = val
-        return val
-    end
+function greenwich:Get(key)
+	local store = self.name
+	if not (not cache[store .. key]) then
+		return cache[store .. key]
+	else
+		local val = db:GetAsync(store .. key)
+		cache[store .. key] = val
+		return val
+	end
 end
 
-function dbFunctions:Delete(store, key)
-    store = store.name
-    addToQueue(
-        function()
-            db:RemoveAsync(store .. key)
-        end
-    )
-    cache[store .. key] = nil
-    return true
+function greenwich:Delete(key)
+	local store = self.name
+	addToQueue(
+		function()
+			db:RemoveAsync(store .. key)
+		end
+	)
+	cache[store .. key] = nil
+	return true
 end
 
-function dbFunctions:Has(store, key)
-    store = store.name
-    if not (not cache[store .. key]) then
-        return not (not cache[store .. key])
-    else
-        return not (not db:GetAsync(store .. key))
-    end
+function greenwich:Has(key)
+	local store = self.name
+	if not (not cache[store .. key]) then
+		return not (not cache[store .. key])
+	else
+		return not (not db:GetAsync(store .. key))
+	end
 end
 
-function dbFunctions:Save(store, key)
-    store = store.name
-    if cache[store .. key] == nil then
-        return false
-    else
-        addToQueue(
-            function()
-                db:SetAsync(store .. key, cache[store .. key])
-            end
-        )
-        return true
-    end
+function greenwich:Save(key)
+	local store = self.name
+	if cache[store .. key] == nil then
+		return false
+	else
+		addToQueue(
+			function()
+				db:SetAsync(store .. key, cache[store .. key])
+			end
+		)
+		return true
+	end
 end
 
-function dbFunctions:Fetch(store, key)
-    store = store.name
-    addToQueue(function()
-        local value = db:GetAsync(store .. key)
-        cache[store .. key] = value
-    end)
-    return true
+function greenwich:Fetch(key)
+	local store = self.name
+	addToQueue(function()
+		local value = db:GetAsync(store .. key)
+		cache[store .. key] = value
+	end)
+	return true
 end
 
-function greenwich:EndQueue()
-    for k, v in pairs(queue) do
-        local s, e =
-            pcall(
-            function()
-                v()
-            end
-        )
-    end
+function greenwich.EndQueue()
+	for k, v in pairs(queue) do
+		local s, e =
+			pcall(
+				function()
+				v()
+			end
+			)
+	end
 end
 
 coroutine.wrap(
-    function()
-        while wait(10) do
-            if #queue > 0 then
-                for k, v in pairs(queue) do
-                    local s, e =
-                        pcall(
-                        function()
-                            v()
-                        end
-                    )
-                    if e or not s then
-                        break
-                    else
-                        queue[k] = nil
-                    end
-                end
-            end
-        end
-    end
+	function()
+		while wait(10) do
+			if #queue > 0 then
+				for k, v in pairs(queue) do
+					local s, e =
+						pcall(
+							function()
+							v()
+						end
+						)
+					if e or not s then
+						break
+					else
+						queue[k] = nil
+					end
+				end
+			end
+		end
+	end
 )()
 
 -- Returning everything.
